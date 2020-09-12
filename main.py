d3daaa0f70fb4ef334dfcfbce03e66c569705f76
Slap Slap <39847299+MaxDiscord@users.noreply.github.com>
2020-09-12 14:36:34 -0700

    Efficiency fixes

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import datetime
 import time
 import sys
 pm25=True
-write = 0
-prev = []
-current = []
 timedelay = input('Delay (in seconds) between each output to CSV? (leave empty for 300) ')
 if timedelay == '':
 	timedelay = 300
@@ -18,8 +15,6 @@ url = "https://www.iqair.com/us/usa/washington/mercer-island?utm_source=homepage
 weather = 'https://weather.com/weather/today/l/2abe70d2c4e189db21e5a4a82fe9e0e90f4d9c39ba36b4365df44a85caf79fa8'
 while True:
 	try:
-		f = open('belleviewcollege.txt','a+')
-		mystring = '2020:9:11,04:01:02'
 		current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 		ctime = str(current_time).split (' ')
 		datee = ctime[0]
@@ -38,16 +33,12 @@ while True:
 
 		test = ''
 		temp = re.findall(r'\d+', str(mydivs))
-		prev = 0
 		test = soup.findAll('div', {'class' : 'table-wrapper'})
-
-		allow = True
-		if allow == True:
-				print ('Current Date:', datee)
-				print ('Current Time:', currenttime)
-				print ('Current Air Quality:', temp[4])
-				print ('Current Temperature: ', tempss[2])
-				print ('\n')
+		print ('Current Date:', datee)
+		print ('Current Time:', currenttime)
+		print ('Current Air Quality:', temp[4])
+		print ('Current Temperature: ', tempss[2])
+		print ('\n')
 		time.sleep(timedelay)
 	except KeyboardInterrupt:
 		sys.exit()
